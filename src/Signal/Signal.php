9d0d055927d87223aff9236297eac4df8cae0803
DarthCorvidus <github@vm01.telton.de>
2023-06-07 22:12:15 +0200

    I changed Signal into a Singleton. pcntl_signal can be accessed globally as well, and to my opinion, there is no drawback in Signal being a Singleton here.

--- src/Signal/Signal.php
+++ src/Signal/Signal.php
@@ -6,11 +6,24 @@
  * With Signal, it is possible to define more than one SignalHandler for each
  * signal. A catchall - Signal::call() - will then loop over them and call them
  * one by one.
+ * 
+ * I am no fan of Singletons, but I think that a singleton is ok here, as the
+ * native functions work on the global scope as well, so there is no disadvantage
+ * over using pcntl_signal; but having to do some sort of dependency injection
+ * for Signal would be messier.
  */
 class Signal {
 	private $handlers = array();
-	function __construct() {
+	static private $instance;
+	private function __construct() {
 		
+	}
+	
+	static function get(): Signal {
+		if(self::$instance == NULL) {
+			self::$instance = new Signal();
+		}
+	return self::$instance;
 	}
 	
 	function addSignalHandler(int $signal, SignalHandler $handler) {
