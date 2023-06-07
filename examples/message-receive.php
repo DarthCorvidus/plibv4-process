0a4f508dfb8fa96e1c2845f8a874fd923ef3dbe1
DarthCorvidus <github@vm01.telton.de>
2023-06-07 23:48:20 +0200

    message-receive.php now uses MessageListener::onMessage as well.

--- examples/message-receive.php
+++ examples/message-receive.php
@@ -2,17 +2,27 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-function test($signal, $info) {
-	$queue = new SysVQueue(451);
-	if($queue->hasMessage()) {
-		$message = $queue->getMessage();
-		echo $message->getMessage().PHP_EOL;
-		return;
+class Listen implements MessageListener {
+	private $queue;
+	private $signal;
+	function __construct() {
+		pcntl_async_signals(true);
+		$this->queue = new SysVQueue(451);
+		$this->signal = Signal::get();
+		$this->queue->addListener($this->signal, $this);
+	}
+
+	public function onMessage(\Message $message) {
+		echo "Received message ".$message->getMessage();
+	}
+
+	function run() {
+		echo "Listening with PID ".posix_getpid().PHP_EOL;
+		while(true) {
+			sleep(10);
+		}
 	}
 }
-pcntl_async_signals(true);
-pcntl_signal(SIGALRM, "test");
-echo "Listening with PID ".posix_getpid().PHP_EOL;
-while(true) {
-	sleep(10);
-}
+
+$listen = new Listen();
+$listen->run();
