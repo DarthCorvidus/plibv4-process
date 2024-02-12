789e5f60c4810329b0002dad3d77950f6be1c052
Claus-Christoph Küthe <floss@vm01.telton.de>
2024-02-12 19:50:47 +0100

    Testing terminate & deferredTerminate.
    At the moment, the latter test does not make as much sense, as it mostly
    tests Counter, as it is the Timeshared instance that shuts itself down by
    having loop return false.
    
    I'm thinking about a different approach, for which this test is prepared.

--- tests/timeshare/TimeshareTest.php
+++ tests/timeshare/TimeshareTest.php
@@ -19,6 +19,29 @@ class TimeshareTest extends TestCase {
 		$this->assertEquals(1, $timeshare->getProcessCount());
 	}
 	
+	/**
+	 * On each loop, one of the tasks is called in the order added to Timeshare.
+	 */
+	function testLoop() {
+		$timeshare = new plibv4\Timeshare\Timeshare();
+		$count01 = new Counter(500);
+		$count02 = new Counter(1000, 100);
+		$timeshare->addTimeshared($count01);
+		$timeshare->addTimeshared($count02);
+		$timeshare->loop();
+		$this->assertEquals(1, $count01->getCount());
+		$this->assertEquals(0, $count02->getCount());
+		$timeshare->loop();
+		$this->assertEquals(1, $count01->getCount());
+		$this->assertEquals(1, $count02->getCount());
+		$timeshare->loop();
+		$this->assertEquals(2, $count01->getCount());
+		$this->assertEquals(1, $count02->getCount());
+	}
+	
+	/**
+	 * Run executes until loop() of every Timeshared instance returns false.
+	 */
 	function testRun() {
 		$timeshare = new plibv4\Timeshare\Timeshare();
 		$count01 = new Counter(500);
@@ -29,4 +52,39 @@ class TimeshareTest extends TestCase {
 		$this->assertEquals(500, $count01->getCount());
 		$this->assertEquals(1000, $count02->getCount());
 	}
+	
+	function testTerminate() {
+		$timeshare = new plibv4\Timeshare\Timeshare();
+		$count01 = new Counter(500);
+		$count02 = new Counter(1000);
+		$timeshare->addTimeshared($count01);
+		$timeshare->addTimeshared($count02);
+		$i = 0;
+		while($timeshare->loop()) {
+			$i++;
+			if($i == 94) {
+				$timeshare->terminate();
+			}
+		}
+		$this->assertEquals(47, $count01->getCount());
+		$this->assertEquals(47, $count02->getCount());
+	}
+
+	function testDeferredTerminate() {
+		$timeshare = new plibv4\Timeshare\Timeshare();
+		$count01 = new Counter(500);
+		$count02 = new Counter(1000, 100);
+		$timeshare->addTimeshared($count01);
+		$timeshare->addTimeshared($count02);
+		$i = 0;
+		while($timeshare->loop()) {
+			$i++;
+			if($i == 94) {
+				$timeshare->terminate();
+			}
+		}
+		$this->assertEquals(47, $count01->getCount());
+		$this->assertEquals(100, $count02->getCount());
+	}
+
 }
