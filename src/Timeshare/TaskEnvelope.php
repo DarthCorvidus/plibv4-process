b41395637a5fe793562c9b3dc7798a4a64dccbc8
Claus-Christoph Küthe <floss@vm01.telton.de>
2024-03-13 19:29:28 +0100

    TaskEnvelope is a small wrapper around every instance of Timeshared.
    This is supposed to encapsulate the state of tasks, so it is not necessary
    to keep it in the scheduler. A first example is that the decision whether
    it is necessary to run __tsStart is offloaded to TaskEnvelope::__tsLoop so
    that the Scheduler does not have to care about Timeshared->__tsStart
    altohether.

---
+++ src/Timeshare/TaskEnvelope.php
@@ -0,0 +1,70 @@
+<?php
+namespace plibv4\process;
+class TaskEnvelope {
+	private Timeshared $task;
+	private Timeshare $scheduler;
+	private TimeshareObservers $timesharedObservers;
+	private bool $started = false;
+	function __construct(Timeshare $scheduler, Timeshared $task, TimeshareObservers $observers) {
+		$this->task = $task;
+		$this->taskObservers = $observers;
+		$this->scheduler = $scheduler;
+		$this->taskObservers->onAdd($this->scheduler, $this->task);
+	}
+	
+	function getTimeshared(): Timeshared {
+		return $this->task;
+	}
+	
+	function __tsLoop(): bool {
+		if(!$this->started) {
+			try {
+				$this->task->__tsStart();
+				$this->taskObservers->onStart($this->scheduler, $this->task);
+				$this->started = true;
+			} catch (\Exception $e) {
+				$this->task->__tsError($e, Timeshare::START);
+				$this->taskObservers->onError($this->scheduler, $this->task, $e, Timeshare::START);
+				$this->scheduler->remove($this->task, Timeshare::ERROR);
+			}
+		return true;
+		}
+		try {
+			$result = $this->task->__tsLoop();
+			return $result;
+		} catch (\Exception $e) {
+			$this->task->__tsError($e, Timeshare::LOOP);
+			$this->taskObservers->onError($this->scheduler, $this->task, $e, Timeshare::LOOP);
+			$this->scheduler->remove($this->task, Timeshare::ERROR);
+		return false;
+		}
+	}
+
+	public function __tsError(\Exception $e, int $step): void {
+		$this->task->__tsError($e, $step);
+	}
+
+	public function __tsFinish(): void {
+		$this->task->__tsFinish();
+	}
+
+	public function __tsKill(): void {
+		$this->task->__tsKill();
+	}
+
+	public function __tsPause(): void {
+		$this->task->__tsPause();
+	}
+
+	public function __tsResume(): void {
+		$this->task->__tsResume();
+	}
+
+	public function __tsStart(): void {
+		$this->task->__tsStart();
+	}
+
+	public function __tsTerminate(): bool {
+		return $this->task->__tsTerminate();
+	}
+}
